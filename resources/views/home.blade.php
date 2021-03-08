527b52c119c74cee8c6537d8ababa5f5294d5a87
kignorchan <kignorchan@gmail.com>
2021-03-07 20:45:44 -0500

    New update in Home screen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,20 +4,24 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @foreach (App\Models\Post::orderBy('created_at', 'DESC')->get() as $post)
-                <div class="card">
-                    <div class="card-header">
-                        #{{ $post->id }}  {{ $post->title }} @ {{ $post->created_at }}
+            @if (App\Models\Post::all()->count()==0)
+                <h4>You have no posts yet. <a href="/posts/create">Create</a> your first post.</h4>
+            @else
+                @foreach (App\Models\Post::orderBy('created_at', 'DESC')->get() as $post)
+                    <div class="card">
+                        <div class="card-header">
+                            #{{ $post->id }}  {{ $post->title }} @ {{ $post->created_at }}
 
-                        @auth
-                            <a href="{{ route('posts.edit',[$post->id]) }}">[Edit]</a>
-                        @endauth
+                            @auth
+                                <a href="{{ route('posts.edit',[$post->id]) }}">[Edit]</a>
+                            @endauth
+                        </div>
+                        <div class="card-body">
+                            {!! nl2br($post->content) !!}
+                        </div>
                     </div>
-                    <div class="card-body">
-                        {!! nl2br($post->content) !!}
-                    </div>
-                </div>
-            @endforeach
+                @endforeach
+            @endif
         </div>
     </div>
 </div>
